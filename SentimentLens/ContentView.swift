fe6aa5c6105ddd77827c17beec3f120c64a23c97
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-03-25 15:29:17 -0300

    Initial Commit

---
+++ SentimentLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SentimentLens
+//
+//  Created by Cem Akkaya on 25/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SentimentLens/SentimentLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentLensApp.swift
+//  SentimentLens
+//
+//  Created by Cem Akkaya on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SentimentLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
