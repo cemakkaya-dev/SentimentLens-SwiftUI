c1d2a6b7d92969fbc25ecec5ee5d90e4ca90bc56
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-03-27 13:41:22 -0300

    Created SentimentAnalyzer methods.

--- SentimentLens/Services/CSVParser.swift
+++ SentimentLens/Services/CSVParser.swift
@@ -9,4 +9,58 @@ import Foundation
 
 struct CSVParser {
     
+    func readCSVFile() -> [Review] {
+        var parsedReviews: [Review] = []
+        
+        guard let fileURL = Bundle.main.url(forResource: "socialMediaReviews", withExtension: "csv") else {
+            print("An error occurred while creating fileURL.")
+            return []
+        }
+        
+        do {
+            let content = try String(contentsOf: fileURL, encoding: .utf8)
+            
+            let reviews = content
+                .components(separatedBy: "\n")
+                .dropFirst()
+                .filter {!$0.isEmpty}
+            
+            for review in reviews {
+                if review.hasPrefix("\"") {
+                    if let index = review.range(of: "\",") {
+                        let text = String(review[review.startIndex ..< index.lowerBound].dropFirst())
+                        let label = String(review[index.upperBound...]).trimmingCharacters(in: .whitespaces)
+                        
+                        if label == "positive" || label == "negative" || label == "neutral" {
+                            let newReview = Review(text: text, label: label)
+                            parsedReviews.append(newReview)
+                        }
+                    }
+                    
+                } else {
+                    
+                    if let index = review.lastIndex(of: ",") {
+                        let text = String(review[review.startIndex ..< index])
+                        let label = String(review[review.index(after: index) ..< review.endIndex]).trimmingCharacters(in: .whitespaces)
+                        
+                        if label == "positive" || label == "negative" || label == "neutral" {
+                            let newReview = Review(text: text, label: label)
+                            parsedReviews.append(newReview)
+                        }
+                        
+                        
+                        
+                    } else {
+                        continue
+                    }
+                }
+            }
+            
+        } catch {
+            print("The file could not be read: \(error)")
+            return []
+        }
+        
+        return parsedReviews
+    }
 }
--- SentimentLens/Services/SentimentAnalyzer.swift
+++ SentimentLens/Services/SentimentAnalyzer.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import CoreML
+
+struct SentimentAnalyzer {
+    
+    let model: socialMediaClassifierModel? = try? socialMediaClassifierModel(configuration: .init())
+    
+    func predict(text: String) -> String {
+        guard let prediction = try? model?.prediction(text: text) else {
+            return "Unknown"
+        }
+        
+        return prediction.label
+    }
+    
+    func analyzeBatch(texts: [String]) -> (positive: Int, negative: Int, neutral: Int) {
+        var counts: (positive: Int , negative: Int , neutral: Int ) = (positive: 0 , negative: 0 , neutral: 0 )
+        
+        for text in texts {
+            let emotion = predict(text: text)
+            switch emotion {
+            case "positive":
+                counts.positive += 1
+            case "negative":
+                counts.negative += 1
+            case "neutral":
+                counts.neutral += 1
+            default:
+                print("Unknown")
+            }
+        }
+        
+        return counts
+    }
+}
